7b47afd9e33a54efaffaa19156f2a56a1eea89df
Maynor Gabriel López Jiménez <maynorlop3z@gmail.com>
2024-05-05 20:07:17 -0600

    primer commit

---
+++ src/main/java/com/iisgt04/lj09002/App.java
@@ -0,0 +1,18 @@
+package com.iisgt04.lj09002;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello Class GT04";
+	}
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println( hw.Hello() );
+    }
+}
+
